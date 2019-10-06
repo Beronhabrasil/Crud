8bda603a2b5c4622b3fb38dd17ba8ed65e3dd66a
Calheiro <33173643+CesarCalheiro@users.noreply.github.com>
2019-10-06 19:09:41 +0200

    Update DB.php

--- DB.php
+++ DB.php
@@ -5,37 +5,27 @@ class DB extends mysqli{
    
     static $db;
     public $getnuwrows;
-     public $readtpage;
+    public $readtpage;
     public $startpage;
     public $cuPage;
     public $nexPage;
     public $limit;
     public $total;
     
-  public function __contruct()
-  {
-      parent::__construct();
-     
-     
-      
+  public function __contruct(){
+   parent::__construct();
   }
     
-    private function connection() {
-        self::$db = new mysqli("localhost", "root", "", "dbkunden");
-       return self::$db;
-        
-        
+    private function connection(){
+     self::$db = new mysqli("localhost", "root", "", "dbkunden");
+     return self::$db;
     }
-    public function GetConn() {
-        return $this->connection(); 
+    public function GetConn(){
+     return $this->connection(); 
     }
-    
-    
-    public function query($sql) {
-        return $this->connection()->query($sql); 
+    public function query($sql){
+     return $this->connection()->query($sql); 
     }
-    
- 
  
 }
 
