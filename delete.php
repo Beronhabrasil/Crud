ff3f87949a2b11f4f42abbd8287bfaeac1e146f0
Calheiro <33173643+CesarCalheiro@users.noreply.github.com>
2019-10-06 19:10:44 +0200

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,15 +1,10 @@
 <?php
 
 $id = $_POST['id'];
-
 include_once 'DB.php';
-
-
 $mysqli = new DB();
 var_dump($mysqli);
-
-        
- $mysqli->GetConn()->query("DELETE FROM `tblkunden` WHERE id='$id'");
+$mysqli->GetConn()->query("DELETE FROM `tblkunden` WHERE id='$id'");
 
 
  
